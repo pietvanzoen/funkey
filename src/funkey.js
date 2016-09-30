c86cd44688b87a5ce62b82477b075a10fa449559
pietvanzoen <pietvanzoen@gmail.com>
2016-09-30 10:29:11 +0200

    Initial dev for simple key matching

--- src/funkey.js
+++ src/funkey.js
@@ -1,2 +1,33 @@
-function funkey() {
+var letters = 'abcdefghijklmnopqrstuvwxyz';
+var alphabetStart = 65;
+
+var numbers = '0123456789';
+var numericStart = 48;
+
+var someSymbols = ';=,-./';
+var someSymbolsStart = 186;
+
+/**
+ * Funkey
+ * @param {KeyboardEvent} event The event to test.
+ * @param {String} keyString The keyboard string to match.
+ * @param {Function} callback Function to invoke on event/keystring match.
+ */
+function funkey(event, keyString, callback) { // eslint-disable-line no-unused-vars
+
+  if (event.keyCode === (alphabetStart + letters.indexOf(keyString))) {
+    callback();
+    return;
+  }
+
+  if (event.keyCode === (numericStart + numbers.indexOf(keyString))) {
+    callback();
+    return;
+  }
+
+  if (event.keyCode === (someSymbolsStart + someSymbols.indexOf(keyString))) {
+    callback();
+    return;
+  }
+
 }
--- test/funkey.spec.js
+++ test/funkey.spec.js
@@ -1,6 +1,42 @@
-/* global funkey */
+/* global funkey funkeyMatchers */
 describe('funkey', function() {
+
+  beforeEach(function() {
+    jasmine.addMatchers(funkeyMatchers);
+  });
+
   it('is defined', function() {
     expect(typeof funkey).toBe('function');
+  });
+
+  describe('(alphanumeric)', function() {
+    it('handles alphabet keys', function() {
+      var alphabet = 'abcdefghijklmnopqrstuvwxyz';
+      var alphabetStart = 65;
+      [].forEach.call(alphabet, function(letter, index) {
+        var keyCode = (alphabetStart + index);
+        expect({keyCode: keyCode}).toTriggerKeyString(letter);
+        expect({keyCode: keyCode + 1}).not.toTriggerKeyString(letter);
+      });
+    });
+
+    it('handles numbers', function() {
+      var numbers = '0123456789';
+      var numbersStart = 48;
+      [].forEach.call(numbers, function(number, index) {
+        var keyCode = (numbersStart + index);
+        expect({keyCode: keyCode}).toTriggerKeyString(number);
+        expect({keyCode: keyCode + 1}).not.toTriggerKeyString(number);
+      });
+    });
+  });
+
+  it('handles single symbols', function() {
+    expect({keyCode: 186}).toTriggerKeyString(';');
+    expect({keyCode: 187}).toTriggerKeyString('=');
+    expect({keyCode: 188}).toTriggerKeyString(',');
+    expect({keyCode: 189}).toTriggerKeyString('-');
+    expect({keyCode: 190}).toTriggerKeyString('.');
+    expect({keyCode: 191}).toTriggerKeyString('/');
   });
 });
