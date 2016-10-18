373f1200089b833b65aef3ff9fbd59523bd01e91
pietvanzoen <pietvanzoen@gmail.com>
2016-10-18 08:51:35 +0200

    Test against dist build during version

--- karma.conf.js
+++ karma.conf.js
@@ -1,11 +1,13 @@
 /* eslint-env node */
 var path = require('path');
 var cfg = require('./package.json').config;
+var testFiles = [].concat(cfg.testDepFiles, cfg.testFiles);
+
 module.exports = function(config) {
   var karmaConfig = {
     basePath: '',
     frameworks: ['jasmine'],
-    files: cfg.srcFiles.concat(cfg.testDepFiles, cfg.testFiles),
+    files: cfg.srcFiles.concat(testFiles),
     reporters: [
       'progress',
       'junit',
@@ -29,6 +31,11 @@ module.exports = function(config) {
     singleRun: false,
     concurrency: Infinity
   };
+
+  if (process.env.TEST_DIST) {
+    console.log('Testing against dist build');
+    karmaConfig.files = ['dist/funkey.min.js'].concat(testFiles);
+  }
 
   if (process.env.COVERALLS_REPO_TOKEN) {
     karmaConfig.reporters.push('coveralls');
